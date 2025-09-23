9c47ba1ebcd3ee74759a0245421cc49d359e35b0
ysnbhb <yassine.bahbib@usmba.ac.ma>
2025-09-23 12:45:48 +0100

    goof

--- src/main/java/_blog/com/_blog/controllers/Login.java
+++ src/main/java/_blog/com/_blog/controllers/Login.java
@@ -3,6 +3,7 @@ package _blog.com._blog.controllers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import _blog.com._blog.model.User;
@@ -11,6 +12,13 @@ import _blog.com._blog.model.User;
 public class Login {
     @PostMapping(path = "/register")
     public ResponseEntity<String> registerUser(@ModelAttribute User user) {
+        System.out.println(user.toString());
+        return ResponseEntity.ok().body("");
+        // return "";
+    }
+
+    @PostMapping(path = "/login")
+    public ResponseEntity<String> loginUser(@RequestBody User user) {
         System.out.println(user.toString());
         return ResponseEntity.ok().body("");
         // return "";
