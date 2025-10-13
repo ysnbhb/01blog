e2eff7869eb01c5005ea5cbfb689fce18c6070b2
ysnbhb <yassine.bahbib@usmba.ac.ma>
2025-10-13 16:24:16 +0100

    frontend

--- frontend/src/app/about/about.ts
+++ frontend/src/app/about/about.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-about',
   imports: [],
   templateUrl: './about.html',
-  styleUrl: './about.css'
+  styleUrl: './about.css' ,
 })
 export class About {
 
